7b4481ee832b618eb789be142534174640a734f5
Raul Andres Castro <andrescastrodevia@gmail.com>
2020-03-09 09:20:03 -0400

    'se_elimino_campo_value_de_la_tabla_type'

--- database/migrations/2020_03_08_165700_create_types_table.php
+++ database/migrations/2020_03_08_165700_create_types_table.php
@@ -16,7 +16,6 @@ class CreateTypesTable extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 50)->unique()->nullable();
-            $table->string('value', 100)->nullable();
             $table->string('category', 50)->nullable();
             $table->timestamps();
         });
